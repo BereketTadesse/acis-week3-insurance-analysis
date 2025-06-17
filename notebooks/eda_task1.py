072db404336049ea969ac2dd3d54f1980eaccb0f
BereketTadesse <berekettadesse1244@gmail.com>
2025-06-17 17:20:00 +0300

    Task 3: Hypothesis testing and business interpretation

---
+++ notebooks/eda_task1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+# 📂 Load the Excel file
+file_path = r"C:\Users\Dar Computers\Desktop\10Acadamy\week 3\data\MachineLearningRating_v3.xlsx"  # update the path if needed
+df = pd.read_excel(file_path)
